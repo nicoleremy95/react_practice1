4dc65a103fad54b13be6be3d1249e8d6dc995041
Nicole Remy <nicoleremy95@gmail.com>
2020-11-03 18:34:01 -0800

    added comments to understand steps

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//Hello is the component name
 class Hello extends React.Component {
+	//render method has to encompass return
 	render() {
 		return (
+			//must wrap in div because you can only return one element in a component
 			<div>
 				<h1>Hello there!</h1>
 				<h1>Hello there!</h1>
@@ -11,4 +14,5 @@ class Hello extends React.Component {
 	}
 }
 
+//render the Hello component where the div ID is root
 ReactDOM.render(<Hello />, document.getElementById('root'));
