599116622e068326cb1bf88ce572019c433c0c1a
Nicole Remy <nicoleremy95@gmail.com>
2020-11-03 19:08:22 -0800

    seperated folders, started jsx practice folder and code

--- first_component/index.js
+++ first_component/index.js
@@ -20,10 +20,8 @@
 function Hello(){
 	return (
 		<div>
-			<h1>Hello there!</h1>
-			<h1>Hello there!</h1>
-			<h1>Hello there!</h1>
-			<h1>Hello there!</h1>
+			<h1>panda!</h1>
+			<img src="https://static01.nyt.com/images/2020/08/16/reader-center/14-panda-baby/14-panda-baby-videoSixteenByNineJumbo1600.jpg" alt="panda"/>
 		</div>
 	)
 }
---
+++ jsx/index.js
@@ -0,0 +1,11 @@
+const { realpathSync } = require("fs");
+
+class JSXDemo extends realpathSync.Component {
+    render(){
+        return(
+
+        )
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
