19c58531a7f91c74d02a959b0d28049009b81fad
Nicole Remy <nicoleremy95@gmail.com>
2020-11-03 19:11:49 -0800

    added a random mood selector

--- jsx/index.js
+++ jsx/index.js
@@ -1,8 +1,16 @@
 const { realpathSync } = require("fs");
 
+function getMood(){
+    const moods = ["angry", 'sad', 'happy', 'frantic']
+    return moods [Math.floor(Math.random() * moods.length)]
+}
 class JSXDemo extends realpathSync.Component {
     render(){
         return(
+            <div>
+                {/* references and excutes a function called getMood */}
+                <h1>My current mood: {getMood()}</h1>
+            </div>
 
         )
     }
