1875a88d0d7e717445b98566e6db842ff13d1571
Nicole Remy <nicoleremy95@gmail.com>
2020-11-03 18:36:20 -0800

    added function based component method

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
+//CLASS BASED COMPONENT
 //Hello is the component name
-class Hello extends React.Component {
-	//render method has to encompass return
-	render() {
-		return (
-			//must wrap in div because you can only return one element in a component
-			<div>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-				<h1>Hello there!</h1>
-			</div>
-		);
-	}
-}
+// class Hello extends React.Component {
+// 	//render method has to encompass return
+// 	render() {
+// 		return (
+// 			//must wrap in div because you can only return one element in a component
+// 			<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
 
+
+//FUNCTION COMPONENT
+function Hello(){
+	return (
+		<div>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+		</div>
+	)
+}
 //render the Hello component where the div ID is root
 ReactDOM.render(<Hello />, document.getElementById('root'));
