0391a0ea39c58742165d1424f2a51360b493c3a4
Nicole Remy <nicoleremy95@gmail.com>
2020-11-05 19:42:02 -0800

    added more properties to class App

--- props/App.js
+++ props/App.js
@@ -2,8 +2,21 @@ class App extends React.Component {
     render(){
         return (
             <div>
-                <Hello to="Ringo" from ="Paul"/>
-                <Hello to="Cher" from ="Sunny"/>
+                <Hello 
+                    to="Ringo" 
+                    from ="Paul" 
+                    age ={3}
+                    data = {[1,2,3,4,5]}
+                    isFunny={true}
+                    bangs = {2}
+                />
+                <Hello 
+                    to="Cher" 
+                    from ="Sunny" 
+                    age ={5}
+                    data = {[1,2,3,4,5]}
+                    isFunny= {false}
+                />
             </div>
         )
     }
--- props/Hello.js
+++ props/Hello.js
@@ -2,11 +2,15 @@
 
 class Hello extends React.Component {
 	render() {
+		let bangs = "!".repeat(this.props.bangs)
 		return (
 			<div>
                 <h1>hi: {this.props.to}</h1>
-                <h1>from: {this.props.from}</h1>
-		    </div>
+                <h1>from: {this.props.from}</h1> {bangs}
+				<p>I am writing to you because I have a dog who is {this.props.age} years old and I would love to find him a new home.</p>
+				{/* {isFunny? <p>this is so funny because I know you already have 12 dogs, but I think you would love him!</p>: <p>I know this would be your first dog</p>} */}
+		    
+			</div>
 		);
     }
 }
