43e42c1fc06dc6a8ba8fb26408406e00d4aa0e1b
Nicole Remy <nicoleremy95@gmail.com>
2020-11-23 12:45:33 -0800

    initializing state example

--- state/Game.js
+++ state/Game.js
@@ -1,5 +1,19 @@
 class Game extends React.Component{
-
+    //initializing state
+    constructor(props){
+        super(props);
+        this.state = {
+            score: 0,
+            gameOver: false
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>Your score is: {this.state.score}</h1>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<Game/>, document.getElementById('root'))
