fe8587dcda69b84fe933a9bd04ef69276ae8ea58
Nicole Remy <nicoleremy95@gmail.com>
2020-11-23 14:39:01 -0800

    alternate syntax for initialize state

--- state/Game.js
+++ state/Game.js
@@ -1,11 +1,29 @@
+// class Game extends React.Component{
+//     //initializing state
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             score: 0,
+//             gameOver: false
+//         }
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Your score is: {this.state.score}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<Game/>, document.getElementById('root'))
+
+//ALTERNATE SYNTAX
 class Game extends React.Component{
     //initializing state
-    constructor(props){
-        super(props);
-        this.state = {
-            score: 0,
-            gameOver: false
-        }
+    state = {
+        score: 0,
+        gameOver: false
     }
     render(){
         return(
